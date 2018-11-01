8b1a7da6027a307f6d652925719dcc7949f20094
Scott Kidder <scott.kidder@ilq.com>
2018-11-01 15:12:17 -0400

    chore(deps): upgrade to my fork of style-loader

--- dist/jquery.card.js
+++ dist/jquery.card.js
@@ -582,12 +582,16 @@ var card =
 	};
 
 	var isOldIE = memoize(function () {
-		// Test for IE <= 9 as proposed by Browserhacks
-		// @see http://browserhacks.com/#hack-e71d8692f65334173fee715c222cb805
 		// Tests for existence of standard globals is to allow style-loader
 		// to operate correctly into non-standard environments
 		// @see https://github.com/webpack-contrib/style-loader/issues/177
-		return window && document && document.all && !window.atob;
+	  if (typeof document === 'undefined' || typeof window === 'undefined') {
+	    return false;
+	  }
+
+		// Test for IE <= 9 as proposed by Browserhacks
+		// @see http://browserhacks.com/#hack-e71d8692f65334173fee715c222cb805
+		return document.all && !window.atob;
 	});
 
 	var getTarget = function (target, parent) {
